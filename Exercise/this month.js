560dd654035687974a261dbd70409a017d352249
ywhkr <dnsghk2@gmail.com>
2022-03-09 19:04:41 +0900

    this month.js modify

--- Exercise/this month.js
+++ Exercise/this month.js
@@ -1,4 +1,4 @@
-/* 
+/*
 
   다음과 같은 조건을 만족하는 월간 달력을 만들어 주세요!
   
@@ -22,8 +22,8 @@
   +---------------------------------------------+
   |  27  |  28  |  29 |  30  |      |     |     |
   +---------------------------------------------+
-*/
 
+*/
 const body = document.querySelector('body');
 const table = document.createElement('table');
 const thead = document.createElement('thead');
@@ -35,41 +35,44 @@ const firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
 const lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 const days = ['일', '월', '화', '수', '목', '금', '토'];
 
+
 body.appendChild(table);
 table.appendChild(thead);
 table.appendChild(tbody);
 tr.push(document.createElement('tr'));
 thead.appendChild(tr[0]);
 
-// 요일 넣을 칸 만듬
+
 for (let i = 0; i < 7; i++) {
-  th.push(document.createElement('th'));
-  tr[0].appendChild(th[i]);
-  //css
-  th[i].style.border = '1px solid black';
-  th[i].style.width = '100px';
-  th[i].style.height = '50px';
+    th.push(document.createElement('th'));
+    tr[0].appendChild(th[i]);
+    //css
+    th[i].style.border = '1px solid black';
+    th[i].style.width = '90px';
+    th[i].style.height = '40px';
 }
+
 
 for (let i = 1; i < 6; i++) {
-  tr.push(document.createElement('tr'));
-  tbody.appendChild(tr[i]);
-  for (let j = i * 7; j < 7 + i * 7; j++) {
-    th.push(document.createElement('th'));
-    tr[i].appendChild(th[j]);
-    //css
-    th[j].style.border = '1px solid black';
-    th[j].style.width = '90px';
-    th[j].style.height = '40px';
-  }
-}
+        tr.push(document.createElement('tr'));
+        tbody.appendChild(tr[i]);
+        for (let j = i * 7; j < 7 + i * 7; j++) {
+            th.push(document.createElement('th'));
+            tr[i].appendChild(th[j]);
+            //css
+            th[j].style.border = '1px solid black';
+            th[j].style.width = '90px';
+            th[j].style.height = '40px';
+        }
+    }
 
-//첫번째 줄에 요일 배정
+
 for (let i = 0; i < 7; i++) {
-  th[i].textContent = days[i];
+    th[i].textContent = days[i];
 }
 
-// 숫자 채우기
+
 for (let i = 0; i < lastDay.getDate(); i++) {
-  th[firstDay.getDay() + 7 + i].textContent = i + 1;
+    th[firstDay.getDay() + 7 + i].textContent = i + 1;
 }
+
